5d7f2b206497539a97c2846cd9992251fc2ec119
tananndart <finaldev@mail.ru>
2017-06-29 10:39:40 +0300

    [new] Initial commit from BallsAndHolesGame

---
+++ BallsAndHolesGame/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "BallsAndHolesGame.h"
+
+using namespace std;
+
+void main()
+{
+	/*
+	BallsAndHolesGamePtr game = make_shared<BallsAndHolesGame>();
+
+	game->create_board(4, 4);
+
+	game->create_ball(1, 1, 1);
+	game->create_ball(2, 0, 3);
+
+	game->create_hole(1, 0, 0);
+	game->create_hole(2, 3, 2);
+
+	game->draw_board(cout);
+	*/
+	char ch;
+	cin >> ch;
+}
